842b9379f7829a80ded8190ba64f1565d3d8eb72
Kenneth <kenwong.500@gmail.com>
2020-05-08 12:14:39 +0800

    Finished

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -4,63 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String video = "rabbid";
-    private VideoView videoview;
-
-    private Uri getMedia(String medianame)
-    {
-        return Uri.parse("android.resource://" + getPackageName() + "/raw/" + medianame);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        videoview = findViewById(R.id.videoView);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.baby_laughing);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setVolume(100, 100);
+        mediaPlayer.start();
+        Toast.makeText(MainActivity.this, "Playing Music", Toast.LENGTH_SHORT).show();
+        mediaPlayer.release();
+        mediaPlayer = null;
 
-//        For the pause and play button
-//        MediaController controller = new MediaController(this);
-//        controller.setMediaPlayer(videoview);
-//        videoview.setMediaController(controller);
-    }
 
-    private void initializePlayer()
-    {
-        Uri videouri = getMedia(video);
-        videoview.setVideoURI(videouri);
-        videoview.start();
-
-        videoview.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+        final VideoView view = findViewById(R.id.videoView);
+        view.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        view.start();
+        view.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
-                Toast.makeText(MainActivity.this, "Playback Complpeted", Toast.LENGTH_SHORT).show();
-                videoview.seekTo(3);
+                Toast.makeText(MainActivity.this, "Playback Completed", Toast.LENGTH_SHORT).show();
+                view.seekTo(3);
             }
         });
-    }
 
-    private void releasePlayer()
-    {
-        videoview.stopPlayback();
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        initializePlayer();
-    }
-
-    @Override
-    protected void onStop()
-    {
-        super.onStop();
-        releasePlayer();
+        //For the pause and play button
+//        MediaController controller = new MediaController(this);
+//        controller.setMediaPlayer(view);
+//        view.setMediaController(controller);
     }
 }
